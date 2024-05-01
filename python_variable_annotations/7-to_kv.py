9fd6d5d6838bc41c1d195e47f8d8d025222d00f7
gunay <gqasimova93@gmail.com>
2024-05-01 20:51:38 +0400

    complex types

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Python - Variable Annotations'''
+from typing import List, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+    '''tuple'''
+    return (k, v*v)
