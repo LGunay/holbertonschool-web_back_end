a180518f2be346584fc0d00b02518fd0474b7c02
gunay <gqasimova93@gmail.com>
2024-05-12 14:00:35 +0400

    all

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Simple function"""
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """index_range"""
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
